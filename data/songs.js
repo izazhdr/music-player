faecbebe1ae6dbc05df18084038df5234f39b95b
izazhdr <izazhaidermail@gmail.com>
2025-07-10 19:21:03 +0500

    Bug fixes V1

--- data/songs.js
+++ data/songs.js
@@ -13,57 +13,64 @@ export const songs = [{
     audio: "media/Losing My Mind - NEFFEX.mp3",
     artist: "NEFFEX"
   }, {
-    id: "5a8d80dd-5f50-4f6b-80ab-88cd5df754fc",
+    id: "9f173155-4c60-49e5-a1c0-5bfa6b6f4b3d",
     placementId: 3,
+    image: "media/dyalla.png",
+    name: "GTA Type Beat",
+    audio: "media/GTA Type Beat - Dyalla.mp3",
+    artist: "Dyalla"
+  }, {
+    id: "5a8d80dd-5f50-4f6b-80ab-88cd5df754fc",
+    placementId: 4,
     image: "media/neffex.png",
     name: "My Way",
     audio: "media/My Way - NEFFEX.mp3",
     artist: "NEFFEX"
   }, {
     id: "104f88dc-dba1-4f84-96d2-c3af9c0f5a7f",
-    placementId: 4,
+    placementId: 5,
     image: "media/neffex.png",
     name: "No Filter",
     audio: "media/No Filter - NEFFEX.mp3",
     artist: "NEFFEX"
   }, {
     id: "42a1e3c9-f45d-416f-bdbd-93e5209df1ce",
-    placementId: 5,
+    placementId: 6,
     image: "media/neffex.png",
     name: "Retribution",
     audio: "media/Retribution - NEFFEX.mp3",
     artist: "NEFFEX"
   }, {
     id: "b92c22f1-3380-4c72-8f5e-267d7ccaa31d",
-    placementId: 6,
+    placementId: 7,
     image: "media/neffex.png",
     name: "6 Shots",
     audio: "media/6 Shots - NEFFEX.mp3",
     artist: "NEFFEX"
   }, {
     id: "30b1a488-e015-40f5-853b-47e8020f5c0f",
-    placementId: 7,
+    placementId: 8,
     image: "media/neffex.png",
     name: "Enough",
     audio: "media/Enough - NEFFEX.mp3",
     artist: "NEFFEX"
   }, {
     id: "f30d59f5-21d5-489c-b49e-31a52369877b",
-    placementId: 8,
+    placementId: 9,
     image: "media/neffex.png",
     name: "Get Through",
     audio: "media/Get Through - NEFFEX.mp3",
     artist: "NEFFEX"
   }, {
     id: "d63a7b3c-4e1f-42c7-9971-79052e775271",
-    placementId: 9,
+    placementId: 10,
     image: "media/neffex.png",
     name: "Go!",
     audio: "media/Go! - NEFFEX.mp3",
     artist: "NEFFEX"
   }, {
     id: "720c25c5-6511-403c-95be-4f295f3c34c6",
-    placementId: 10,
+    placementId: 11,
     image: "media/neffex.png",
     name: "Good Day",
     audio: "media/Good Day (Wake Up) - NEFFEX.mp3",
